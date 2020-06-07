812e8bf5dcfd09f732ad9d0c0f64ed622415f475
natalia <natalia.d@devhouse.pro>
2020-06-08 00:03:27 +0300

    Added config and test suite

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # language_autotest
+Repository with final assignment of 3.6 lesson on stepik course https://stepik.org/course/575/syllabus
+For launch use command
+`pytest --language=es test_items.py`
---
+++ test_items.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_check_buy_button(browser):
+    browser.get(link)
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "btn-add-to-basket"))
+    )
+    assert browser.find_element_by_css_selector(".btn-add-to-basket").is_displayed()
